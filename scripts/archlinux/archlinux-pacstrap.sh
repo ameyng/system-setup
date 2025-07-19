8a551ea8ede3a172bd147dac787ff834a2e3c542
Amey Narendra Gangal <ameyngangal@gmail.com>
2025-07-20 01:16:58 +0530

    Many updates and re-organization.

---
+++ scripts/archlinux/archlinux-pacstrap.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Exit on any error.
+# Exit if any unset variable is used/referenced.
+set -eu
+
+# Install necessary/essential packages.
+echo 'Installing the required packages.'
+pacstrap -i -P /mnt \
+               base \
+               base-devel \
+               bash \
+               bash-completion \
+               cryptsetup \
+               efibootmgr \
+               elinks \
+               exfatprogs \
+               git \
+               intel-ucode \
+               iptables \
+               iwd \
+               linux-firmware-cirrus \
+               linux-firmware-intel \
+               linux-firmware-nvidia \
+               linux-firmware-other \
+               linux-firmware-realtek \
+               linux-firmware-whence \
+               linux-lts \
+               linux-lts-headers \
+               lvm2 \
+               man-db \
+               mkinitcpio \
+               networkmanager \
+               openssh \
+               openssl \
+               pacman-contrib \
+               pkgfile \
+               plymouth \
+               powertop \
+               reflector \
+               sudo \
+               terminus-font \
+               tuned \
+               tuned-ppd \
+               unzip \
+               usbutils \
+               vim
+
+# Exit with a successful error code.
+exit 0
