d10fa029f7a21f4d02dd785a976c2590678c48a2
Amey Narendra Gangal <ameyngangal@gmail.com>
2025-09-01 20:34:52 +0530

    Few updates

--- grub/configure-grub.sh
+++ grub/configure-grub.sh
@@ -18,14 +18,14 @@ if [ 0 -ne "${is_grubby_installed}" ]; then
 else
 
   echo 'Making the following changes to all kernel entries in the GRUB bootloader:'
+  echo " - Remove the 'rhgb' parameter as it is old and obsolete and replace it with the newer 'splash' parameter."
   echo ' - Suppress console output during boot.'
-  echo ' - Disable PCIE active state power management as it is often broken.'
   echo ' - Disable all watchdogs for power-saving.'
   echo ' - Disable all open source drivers for NVIDIA GPUs.'
   echo ' - Enable the kernel-mode setting for the proprietary NVIDIA driver.'
   echo ' - Enable a dedicated framebuffer device for the proprietary NVIDIA driver.'
-  echo " - Remove the 'rhgb' parameter as it is old and obsolete and replace it with the newer 'splash' parameter."
-  sudo grubby --update-kernel=ALL --args='root=/dev/mapper/ROOT quiet pcie_aspm=off nowatchdog nmi_watchdog=0 modprobe.blacklist=iTCO_wdt,sp5100_tco,nouveau,nova_core,nova_drm rd.driver.blacklist=iTCO_wdt,sp5100_tco,nouveau,nova_core,nova_drm nvidia-drm.modeset=1 nvidia-drm.fbdev=1 splash' --remove-args='rhgb \$tuned_params'
+  echo ' - Enable performance tuning capabilities for the AMD GPU drivers.'
+  sudo grubby --update-kernel=ALL --remove-args='rhgb \$tuned_params' --args='quiet splash nowatchdog nmi_watchdog=0 modprobe.blacklist=iTCO_wdt,sp5100_tco,nouveau,nova_core,nova_drm rd.driver.blacklist=iTCO_wdt,sp5100_tco,nouveau,nova_core,nova_drm nvidia-drm.modeset=1 nvidia-drm.fbdev=1 amdgpu.ppfeaturemask=0xffffffff'
 
   echo ''
   echo 'Printing the adjusted kernel command-line parameters from the default GRUB entry.'
